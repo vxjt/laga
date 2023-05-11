498d1e83992b350ad8d9763f3ab54e4a3b1950ac
vxjt <nsandborn@gmail.com>
2023-05-11 18:34:16 -0500

    aa

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,22 @@ import { logit } from './app.ts'
 const search = document.querySelector('#search') as HTMLElement;
 const app = document.querySelector('#app') as HTMLElement;
 const dev = document.querySelector('#dev') as HTMLElement;
-const html = {raw: {}}
-
-let ystring = ''
+const html = {raw: {}, str: ``}
+const apis = {
+  global: {
+    rpc: 1200
+  },
+  rcam: {
+    name: 'rcam',
+    ready: 0,
+    output: new Map()
+  },
+  rcna1: {
+    name: 'rcna1',
+    ready: 0,
+    output: new Map()
+  }
+}
 
 const headrc = {
   headers: {
@@ -24,13 +37,22 @@ function init(): void {
 
 async function bigsearch(event: KeyboardEvent) {
   if (event.key == "Enter") {
+    app.innerHTML = `<center>sec</center>`
     let val = (event.target as HTMLInputElement).value
     if (val.length > 3) {
+      //method 1 works
       const resp1 = await fetch(`http://localhost:5173/rcna1/lol/summoner/v4/summoners/by-name/${val}`, headrc)
       const suminfo = await resp1.json()
+      //method 2 test
+      apis.rcna1.output.set(`/lol/summoner/v4/summoners/by-name/${val}`, null)
+      setTimeout(() => runqueue(apis.rcna1), apis.rcna1.ready)
+      //end
       if (suminfo.puuid) {
         const resp2 = await fetch(`http://localhost:5173/rcam/lol/match/v5/matches/by-puuid/${suminfo.puuid}/ids?start=0&count=5`, headrc)
         const matches = await resp2.json()
+        for (const i in matches) {
+          apis.rcam.output.set(`/lol/match/v5/matches/${matches[i]}`, null)
+        }
         const resp3 = await fetch(`http://localhost:5173/rcam/lol/match/v5/matches/${matches[0]}`, headrc)
         const rjson = await resp3.json()
         Object.assign(bigdata, rjson)
@@ -38,38 +60,64 @@ async function bigsearch(event: KeyboardEvent) {
         Object.assign(bigdata, suminfo)
       }
       fillbigdata(bigdata)
+      console.log(apis)
     }
   }
 }
 
+function runqueue(api: any) {
+  const testiter = api.output.entries()
+  console.log(testiter.next().value)
+
+  //const tester = api.output.find((test:any) => test)
+  /*
+  for (const a in api.output) {
+    console.log(a)
+  }*/
+  //console.log(tester)
+  /*
+  async (value: any, key: any) => {
+    if (value) {
+      console.log(`already had ${key} ${value}`)
+    } else {
+      console.log(`calling out for ${key} ${value}`)
+      const tf = await fetch(`http://localhost:5173/${api.name}${key}`, headrc)
+      api.output.set(key, await tf.json())
+      api.ready = Date.now() + apis.global.rpc
+    }
+  }*/
+}
+
 function rrender(rm: {raw: any}) {
+  let ys = ``
   for (const p in rm.raw) {
     switch (typeof rm.raw[p]) {
       case 'object':
-        ystring += `<details><summary><span class="mono">${p}</span></summary>`
-        rrender({raw: rm.raw[p]})
-        ystring += `</details>`
+        ys += `<details><summary><span class="mono">${p}</span></summary>`
+        ys += rrender({raw: rm.raw[p]})
+        ys += `</details>`
         break;
       case 'string':
-        ystring += `&emsp;<span class="mono">${p}: "${rm.raw[p]}"</span><br>`
+        ys += `&emsp;<span class="mono">${p}: "${rm.raw[p]}"</span><br>`
         break;
       case 'number':
-        ystring += `&emsp;<span class="mono">${p}: <span class="num">${rm.raw[p]}</span></span><br>`
+        ys += `&emsp;<span class="mono">${p}: <span class="num">${rm.raw[p]}</span></span><br>`
         break;
       case 'boolean':
-        ystring += `&emsp;<span class="mono">${p}: <span class="bool">${rm.raw[p]}</span></span><br>`
+        ys += `&emsp;<span class="mono">${p}: <span class="bool">${rm.raw[p]}</span></span><br>`
         break;
       default:
-        ystring += `&emsp;<span class="error">${typeof rm.raw[p]} & ${p}: ${rm.raw[p]}</span><br>`
+        ys += `&emsp;<span class="error">${typeof rm.raw[p]} & ${p}: ${rm.raw[p]}</span><br>`
     }
   }
+  return ys
 }
 
 async function fillbigdata(pd: any) {
   html.raw = pd
-  ystring = ``
-  rrender(html)
-  dev.innerHTML = ystring
+  html.str = rrender(html)
+  dev.innerHTML = html.str
+  app.innerHTML = `<center>extreme</center>`
 }
 
 init()
